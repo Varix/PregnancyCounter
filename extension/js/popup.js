b1d7ee54cd9a96d73e17457d992fea7cd5602828
koeri <ericom.okd@gmail.com>
2014-01-25 18:29:54 +0900

    Add a function for date input according to the Gregorian calendar

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -1,8 +1,6 @@
 /////////////////////////////////
 // # TODO
 // - 日付入力を過去の日付を入力できないようにしたい。
-// - 月の数字にあわせて、日の表示をコントロールしたい（30,31,うるう年）
-// - jQueryで動的にoptionを生成するようにしたい。
 // - バッヂ表示をタイマー更新で日付変わると同時に更新したい。
 // - 出産予定日を過ぎたら「もう産まれた？」とか軽いメッセージに変える。
 // - 妊娠月週日に合わせたアドバイスやテキストを表示するようにしたい。
@@ -13,6 +11,8 @@
 // - 西暦入力をプルダウンにしたい。
 // - 日付の初期値を今日の日付にしたい。
 // - リセットじゃなくて変更に文言変更する。
+// - 月の数字にあわせて、日の表示をコントロールしたい（30,31,うるう年）
+// - jQueryで動的にoptionを生成するようにしたい。
 /////////////////////////////////
 
 /////////////////////////////////
@@ -94,31 +94,76 @@ var controlPregnancyDate = function(){
 	var TODAY = XDate();
 
 	// 入力可能な年の設定: 今年と翌年の2年分を取得する
-	// HTMLに最初から<option>要素を記述してなくてもjQueryで追加して記述するように変更したい
-	$("#inputYYYY>option:eq(0)").text(TODAY.getFullYear());
-	$("#inputYYYY>option:eq(1)").text(TODAY.getFullYear() + 1);
+	$("#inputYYYY").append($("<option></option>").text(TODAY.getFullYear()));
+	$("#inputYYYY").append($("<option></option>").text(TODAY.getFullYear() + 1));
 
 	// 入力可能な月の設定: 初期値は現在の月
-	// HTMLに最初から<option>要素を記述してなくてもjQueryで追加して記述するように変更したい
 	for(var i = 0; i < 12; i++){
-		$("#inputMM>option:eq(" + i + ")").text(i + 1);
+		$("#inputMM").append($("<option></option>").text(i + 1));
 	};
 	// 今月を初期値として選択
 	var thisMonth = Number(TODAY.getMonth());
 	$("#inputMM>option:eq(" + thisMonth + ")").attr("selected","selected");
 
 	// 入力可能な日の設定: 初期値は現在の日
-	// HTMLに最初から<option>要素を記述してなくてもjQueryで追加して記述するように変更したい
 	// 選択されている月に応じて、入力できる日を制限したい ex.30日までの月, 31日まである月, うる年
-	for(var j = 0; j < 31; j++){
-		$("#inputDD>option:eq(" + j + ")").text(j + 1);
-	};
+	controlDay();
+
+	// 年または月の表示が変わるたびに日の表示数を制御
+	$("#inputYYYY").change(function(){
+		controlDay();
+	});
+	$("#inputMM").change(function(){
+		controlDay();
+	});
+
 	// 今日を初期値として選択
 	var thisDay = Number(TODAY.getDate()) - 1;
 	$("#inputDD>option:eq(" + thisDay + ")").attr("selected","selected");
 
 	// console.log(TODAY.getMonth() + 1);
 	// console.log($("#inputMM>option:eq(0)").text(1));
+}
+
+/////////////////////////////////
+// 設定された月に応じて日の表示数（28 or 29 or 30 or 31）を制御
+/////////////////////////////////
+var controlDay = function(){
+
+	// 一度option内を空にする
+	$("#inputDD").empty();
+
+	// 2月の場合
+	if($("#inputMM").val() == 2){
+
+		// うるう年の場合（西暦が 4,100,400 のいづれでも割り切れる場合）
+		if($("#inputYYYY").val() % 4 == 0 && $("#inputYYYY").val() % 100 == 0 && $("#inputYYYY").val() % 400 == 0){
+		//if($("#inputYYYY").val() == 2014){
+			// console.log("2014年だお");
+			for(var i = 0; i < 29; i++){
+				$("#inputDD").append($("<option></option>").text(i + 1));
+			};
+		// うるう年ではない場合
+		}else{
+			// console.log("2015年だお");
+			for(var i = 0; i < 28; i++){
+				$("#inputDD").append($("<option></option>").text(i + 1));
+			};
+		}
+
+	// 30日までしかない月（4,6,9,11月）
+	}else if($("#inputMM").val() == 4 || $("#inputMM").val() == 6 || $("#inputMM").val() == 9 || $("#inputMM").val() == 11){
+		for(var i = 0; i < 30; i++){
+			$("#inputDD").append($("<option></option>").text(i + 1));
+		};
+
+	// 31日まである月（それ以外）
+	}else{
+		for(var i = 0; i < 31; i++){
+			$("#inputDD").append($("<option></option>").text(i + 1));
+		};
+	}
+
 }
 
 /////////////////////////////////
